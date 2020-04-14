ae1d5a9a9f036c0cddaa0cd967460be6ed62a35d
Wodnyan <gergoalbert.gta@gmail.com>
2020-04-14 14:10:10 +0300

    Finished the project

---
+++ precache-manifest.8532e45cbfe91c0315eaf52b2108ae47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5496187b4cfdc0a383339e8c26799e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "089b66430e38a0b285d9",
+    "url": "./static/css/main.6edc1c99.chunk.css"
+  },
+  {
+    "revision": "b5acd74324ee1521f1dd",
+    "url": "./static/js/2.58d1ded7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.58d1ded7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089b66430e38a0b285d9",
+    "url": "./static/js/main.b580b5d2.chunk.js"
+  },
+  {
+    "revision": "8bc83efdb58e24790ea0",
+    "url": "./static/js/runtime-main.b3a575e4.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "./static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "./static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "./static/media/icon-remove.ae67c678.svg"
+  }
+]);
